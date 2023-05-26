f87767316f69a79652c46c159f109e9a3c37a49a
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2023-05-26 18:01:49 +0530

    n = input() arr = list(n) s = set(arr) if len(arr) == len(s):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,12 +1,7 @@
-num = input()
-l = len(num)
-c = 0
-for i in range(l):
-    m = i + 1
-    for j in range(m, l):
-        if num[i]==num[j]:
-            c = 1
-if c == 0:
+n = input()
+arr = list(n)
+s = set(arr)
+if len(arr) == len(s):
     print("Unique Number")
-else :
+else:
     print("Not Unique Number")
